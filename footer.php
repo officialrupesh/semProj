64ca14cd3945e8e0e47687e25b7f3337934d30a1
rautrupesh1 <iamrupeshraut1@gmail.com>
2020-09-07 09:34:56 +0545

    yo

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="footer.css ">
+</head>
+<body>
+	<div class="footer">
+		<div class="inner_footer">
+			<hr>
+			<div class="footer_first">
+
+				<h1>Contact</h1>
+				<p>Kings Park, Blacktown, 2148, NSW</p>
+				<p>E/ info@kingstraven.com.au</p>
+				<p>T/ +61 0456346224</p>
+			</div>
+			<div class="footer_first">
+				<h1>Opening Hours</h1>
+				<p>Monday-Friday</p>
+				<p>11am - 11pm</p>
+				<p>Saturnday-Sunday</p>
+				<p>10am - 12pm</p>
+			</div>
+			<div class="footer_first">
+				<h1>Follow Us</h1>
+				<ul>
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			</ul>
+			</div>
+				
+	</div>
+	</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<nav id="logo"><a href="headerFooter.php"><img src="img/ogo.png"></a></nav>
+	<nav id="hf">
+		
+		
+		<ul>
+			<li><a href="index.php">HOME</a></li>
+			<li><a href="menu.php">MENU</a></li>
+			<li><a href="ordOn.php">ORDER ONLINE</a></li>
+			<li><a href="event.php">EVENT</a></li>
+			<li><a href="gallery.php">GALLERY</a></li>
+			<li><a href="aboutUs.php">ABOUT US</a></li>
+			<li><a href="login.php"><i class="fa fa-user-circle"></i></i></a>
+			<!-- <li><a href="index.php">LOGIN</a></li> -->
+		</ul>
+	</nav>
+</body>
+</html>
